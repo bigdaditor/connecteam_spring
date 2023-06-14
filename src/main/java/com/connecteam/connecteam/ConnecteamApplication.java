ba11d96f836f213717e550f754fabd0f3753ab7e
bigdaditor <bigdaditor1015@gmail.com>
2023-06-14 14:17:56 +0900

    init commit

---
+++ src/main/java/com/connecteam/connecteam/ConnecteamApplication.java
@@ -0,0 +1,13 @@
+package com.connecteam.connecteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnecteamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnecteamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/connecteam/connecteam/ConnecteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connecteam.connecteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnecteamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
